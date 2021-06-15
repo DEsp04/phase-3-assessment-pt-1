af5b53e06035914f6fb1290383bafde035f8863a
David Espinal <despinal0425@gmail.com>
2021-06-15 19:16:43 -0400

    working on algos

--- partone.js
+++ partone.js
@@ -1,1 +1,12 @@
 // #1 Data Structure and Algorithms
+function frequencyOfEachWord(str) {
+  let strArray = str.split(" ");
+
+  if (strArray.length === 0) return undefined;
+
+  let dictionary = {};
+
+  for () {
+    
+  }
+}
