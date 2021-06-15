426cb07d2220d3f9f6e3b163aa7f5c1d8a65e9a1
David Espinal <despinal0425@gmail.com>
2021-06-15 19:25:30 -0400

    completed part 1 of the test

--- partone.js
+++ partone.js
@@ -1,12 +1,23 @@
 // #1 Data Structure and Algorithms
 function frequencyOfEachWord(str) {
   let strArray = str.split(" ");
-
+  //if string is "" then return undefined
   if (strArray.length === 0) return undefined;
 
+  //we are going to put the key value pairs that present multiple times in the string
   let dictionary = {};
-
-  for () {
-    
+  //loop into the array of strings
+  for (let i = 0; i < strArray.length; i++) {
+    //assign a word element for each iteration to the word variable
+    let word = strArray[i];
+    //if the word is not present in the dictionary object, then add the word as key and a value of 1
+    //else if the word is present in the dictionary object then add a one to already existing key's value
+    if (dictionary[word] === undefined) {
+      dictionary[word] = 1;
+    } else {
+      dictionary[word] += 1;
+    }
   }
+  //return the object with key values showing the word and its frequencies time in the string
+  return dictionary
 }
